aeef7914942123ebaf66f78a73c72f897039a077
chubnastia <ch.nastia20@gmail.com>
2025-08-18 14:22:45 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/yourticketing/concert_backend/ConcertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourticketing.concert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
